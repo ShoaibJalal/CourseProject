dc037d1ea4b7be010c035c19fc01a2efff7d3c6a
Shoaib Jalal <you@example.com>
2018-11-20 12:35:55 +0200

    adding map and marker to project

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,13 +1,48 @@
 import React, { Component } from "react";
-import { View, Button, StyleSheet, Text } from "react-native";
+import { View, Button, StyleSheet, Dimensions } from "react-native";
+import { MapView } from "expo";
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 60.200692,
+      longitude: 24.934302,
+      latitudeDelta: 0.0322,
+      longitudeDelta:
+        (Dimensions.get("window").width / Dimensions.get("window").height) *
+        0.0322
+    },
+    locationChosen: false
+  };
+  pickLocationHandler = e => {
+    let coords = e.nativeEvent.coordinate;
+
+    this.setState(prevState => {
+      return {
+        focusedLocation: {
+          ...prevState.focusedLocation,
+          latitude: coords.latitude,
+          longitude: coords.longitude
+        },
+        locationChosen: true
+      };
+    });
+  };
   render() {
+    let marker = null;
+    if (this.state.locationChosen) {
+      marker = <MapView.Marker coordinate={this.state.focusedLocation} />;
+    }
     return (
       <View style={styles.container}>
-        <View style={styles.placeholder}>
-          <Text>Map</Text>
-        </View>
+        <MapView
+          initialRegion={this.state.focusedLocation}
+          region={this.state.focusedLocation}
+          style={styles.map}
+          onPress={this.pickLocationHandler}
+        >
+          {marker}
+        </MapView>
         <View style={styles.button}>
           <Button
             title="Use current location"
@@ -24,12 +59,9 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "center"
   },
-  placeholder: {
-    borderWidth: 1,
-    borderColor: "black",
-    backgroundColor: "#eee",
-    width: "80%",
-    height: 150
+  map: {
+    width: "100%",
+    height: 250
   },
   button: {
     margin: 8
